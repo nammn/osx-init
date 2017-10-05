c9ca0dd229ff5cb6b0854f8168c74f585883be9a
Nam Nguyen <nam.nguyen.de@gmail.com>
2017-10-05 22:07:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,18 +44,10 @@ killall Finder 2>/dev/null;
 
 
 # install Xcode Command Line Tools
-# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" -v;
+xcode-select --install
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew install \
-  caskroom/cask/brew-cask \
   brew-cask-completion \
   fish \
   neovim \
